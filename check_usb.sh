777ff0d46a82410b05aebc5d5676d11e659b121f
xss <lonlylovesir@gmail.com>
2016-05-09 09:29:57 +0800

    rtlwifibt_20160509

---
+++ check_usb.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+#Author:	"Sakya Sakyasir@163.com"
+#Copyright (c)
+#Scripts for checking usb exit or not.
+#/usr/share/cros/check_usb.sh
+
+if [ -e /usr/sbin/checkusb ]; then
+/usr/sbin/checkusb
+fi
+
